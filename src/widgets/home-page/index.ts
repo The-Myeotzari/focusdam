a029f580eabaccd2a476e0c541d1f5b007c7701e
OlMinJe <Ol.MinJe@gmail.com>
2026-06-17 17:47:20 +0900

    feat: 신규 유저 전용 홈 화면 구현 #2

--- src/widgets/home-page/index.ts
+++ src/widgets/home-page/index.ts
@@ -1,3 +1,3 @@
-export { ExampleHomePage } from './ui/example-home-page';
 export { HomePage } from './ui/home-page';
+export { NewUserHome } from './ui/new-user-home';
 export { QuickStartMenu } from './ui/quick-start-menu';
