7bf4e782936405f928376fa8db1f79ac3f8a6686
OlMinJe <Ol.MinJe@gmail.com>
2026-06-17 17:43:23 +0900

    feat: 신규 유저 전용 '추천 작은 행동' 구현 파일 Index #2

---
+++ src/features/create-starter/index.ts
@@ -0,0 +1,3 @@
+export { getRandomRecommendedAction } from './lib/get-random-recommended-action';
+export { RECOMMENDED_ACTIONS } from './model/recommended-actions';
+export { RecommendedAction } from './ui/recommended-action';
