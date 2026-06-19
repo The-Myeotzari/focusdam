b1e11e5a913c43a25f7e654946efa1c5451edc4c
OlMinJe <Ol.MinJe@gmail.com>
2026-06-19 13:33:07 +0900

    feat: 홈 화면 신규 혹은 기존 유저에 따라 화면 노출 바뀌도록 구현 #2

--- src/widgets/home-page/ui/home-page.tsx
+++ src/widgets/home-page/ui/home-page.tsx
@@ -1,18 +1,19 @@
-import { NewUserHome, QuickStartMenu } from '@/widgets/home-page';
+import { NewUserHome, QuickStartMenu, ReturningUserHome } from '@/widgets/home-page';
 
-export function HomePage() {
+type Props = {
+  userKind: 'new' | 'returning';
+};
+
+export function HomePage({ userKind }: Props) {
   return (
     <main className="flex gap-8 flex-col px-5 py-4">
-      {/* 신규 회원 전용 홈 화면 */}
-      <NewUserHome />
-
-      {/* 기존 유저 전용 스타터 추천(스타터 미진행인 경우) */}
-
-      {/* 기존 유저 전용 진행 중인 스타터 노출 */}
-
-      {/* 기존 유저 2열 2행 메뉴(스타터 만들기/착수카드/시간카드/금액카드) */}
-
-      {/* 기존 유저 진행 중인 활동 */}
+      {userKind === 'new' ? (
+        // 신규 회원 전용 홈 화면
+        <NewUserHome />
+      ) : (
+        // 기존 회원 전용 홈 화면
+        <ReturningUserHome />
+      )}
 
       {/* (공통) Quick Start */}
       <QuickStartMenu />
